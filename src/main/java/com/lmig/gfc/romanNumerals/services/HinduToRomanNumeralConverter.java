54d3f231ea4682065a76b63a74553b4e24b4c795
Britta Mielke <britta.mielke@libertymutual.com>
2017-11-28 20:56:06 -0500

    finished coding for remainder of unit tests. Assignment ready for submission

--- src/main/java/com/lmig/gfc/romanNumerals/services/HinduToRomanNumeralConverter.java
+++ src/main/java/com/lmig/gfc/romanNumerals/services/HinduToRomanNumeralConverter.java
@@ -6,9 +6,52 @@ public class HinduToRomanNumeralConverter {
 		String roman = "";
 		int remainder = i;
 
-		
+		if (remainder >= 1000) {
+			int timesToLoop = remainder / 1000;
+			while (timesToLoop > 0) {
+				roman = roman + "M";
+				remainder = remainder - 1000;
+				timesToLoop = timesToLoop - 1;
+			}
+		}
+
+		if (remainder >= 900) {
+			roman = roman + "CM";
+			remainder = remainder - 900;
+		}
+		if (remainder >= 500) {
+			roman = roman + "D";
+			remainder = remainder - 500;
+		}
+		if (remainder >= 400) {
+			roman = roman + "CD";
+			remainder = remainder - 400;
+		}
+
+		if (remainder >= 100) {
+			int timesToLoop = remainder / 100;
+			while (timesToLoop > 0) {
+				roman = roman + "C";
+				remainder = remainder - 100;
+				timesToLoop = timesToLoop - 1;
+			}
+		}
+
+		if (remainder >= 90) {
+			roman = roman + "XC";
+			remainder = remainder - 90;
+		}
+		if (remainder >= 50) {
+			roman = roman + "L";
+			remainder = remainder - 50;
+		}
+		if (remainder >= 40) {
+			roman = roman + "XL";
+			remainder = remainder - 40;
+
+		}
 		if (remainder >= 10) {
-			int timesToLoop =remainder/10;
+			int timesToLoop = remainder / 10;
 			while (timesToLoop > 0) {
 				roman = roman + "X";
 				remainder = remainder - 10;
